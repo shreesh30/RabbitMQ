5a9bb33ad83a820b2956dd703334f1a93a383d4a
shreesh30 <shreesharya20@gmail.com>
2026-03-22 22:36:41 +0530

    updated code

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,9 +1,10 @@
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
+import com.rabbitmq.client.*;
 
 public class Consumer {
+    public static String EXCHANGE_NAME="test_exchange";
+    public static String QUEUE_NAME="test_queue";
+    public static String ROUTING_KEY="test_key";
+
     public static void main(String[] args) {
         try {
             ConnectionFactory factory = new ConnectionFactory();
@@ -12,7 +13,27 @@ public class Consumer {
             Connection connection = factory.newConnection();
             Channel channel = connection.createChannel();
 
-            channel.queueDeclare("test_queue", false, false, false, null);
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+            /*
+             * exchange name
+             * type (direct, fanout, topic, headers)
+             */
+
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            /*
+             * queue name,
+             * durable,
+             * exclusive(Only this connection can use the queue, queue is deleted when connection closes),
+             * autoDelete(queue deletes when last consumer disconnects),
+             * arguments*/
+
+            channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+            /*
+             * queue
+             * exchange
+             * routing key
+             */
 
             System.out.println("Waiting for messages...");
 
@@ -21,8 +42,13 @@ public class Consumer {
                 System.out.println(" Received: " + message);
             };
 
-            channel.basicConsume("test_queue", true, deliverCallback, consumerTag -> {
+            channel.basicConsume(QUEUE_NAME, true, deliverCallback, consumerTag -> {
             });
+            /*
+            * queue name
+            * autoAck
+            * deliverCallback(What to do when message arrives)
+            * consumerTag->{}(It is called if consumer is cancelled)*/
         }catch (Exception e){
             System.out.println("Exception: "+e.getMessage());
         }
--- src/main/java/Producer.java
+++ src/main/java/Producer.java
@@ -1,8 +1,13 @@
+import com.rabbitmq.client.BuiltinExchangeType;
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class Producer {
+    public static String EXCHANGE_NAME="test_exchange";
+    public static String QUEUE_NAME="test_queue";
+    public static String ROUTING_KEY="test_key";
+
     public static void main(String[] args) {
         try {
             ConnectionFactory factory = new ConnectionFactory();
@@ -11,11 +16,35 @@ public class Producer {
             Connection connection = factory.newConnection();
             Channel channel=connection.createChannel();
 
-            String queueName = "test_queue";
-            channel.queueDeclare(queueName, false, false, false, null);
+            channel.exchangeDeclare(EXCHANGE_NAME, BuiltinExchangeType.DIRECT);
+            /*
+             * exchange name
+             * type (direct, fanout, topic, headers)
+             */
 
-            String message = "Hello RabbitMQ";
-            channel.basicPublish("", queueName,null, message.getBytes());
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            /*
+             * queue name,
+             * durable,
+             * exclusive(Only this connection can use the queue, queue is deleted when connection closes),
+             * autoDelete(queue deletes when last consumer disconnects),
+             * arguments*/
+
+            channel.queueBind(QUEUE_NAME, EXCHANGE_NAME, ROUTING_KEY);
+            /*
+             * queue
+             * exchange
+             * routing key
+             */
+
+            String message = "Hello via Exchange";
+            channel.basicPublish(EXCHANGE_NAME, ROUTING_KEY,null, message.getBytes());
+            /*
+            * exchange("" means default exchange will be used i.e. direct exchange),
+            * routing key,
+            * props(message meta data)
+            * message.getBytes()(Actual message payload)*/
 
             System.out.println("Sent: "+message);
             channel.close();
